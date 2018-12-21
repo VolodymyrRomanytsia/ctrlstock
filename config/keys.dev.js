06a485849629172abf252cc70580cb07055f86fd
VolodymyrRomanytsia <40672831+VolodymyrRomanytsia@users.noreply.github.com>
2018-12-21 16:26:34 +0200

    Delete keys.dev.js

--- config/keys.dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://ctrlstock94:nazarkantorastock1@ds141328.mlab.com:41328/ctrlstock',
-    jwt: 'dev-jwt',
-    apiKey: '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'
-}
