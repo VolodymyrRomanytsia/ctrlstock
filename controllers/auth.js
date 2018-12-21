f3c2bb8a61a91eedf4e719a8ed34b1ffb26398c3
Володимир Романиця <romanytsiavolodymyr@MacBook-Air-Volodimir.local>
2018-12-21 13:13:58 +0200

    add s

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,7 +88,7 @@ module.exports.forgot = async function(req, res) {
                     subject: 'Password Reset',
                     text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                     'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'http://'+ req.headers.host +'/reset/' + token + '\n\n' +
+                    'https://'+ req.headers.host +'/reset/' + token + '\n\n' +
                     'If you did not request this, please ignore this email and your password will remain unchanged.\n'
                 })
                 res.status(200).json('На ваш email відправлено посилання для відновлення паролю')
